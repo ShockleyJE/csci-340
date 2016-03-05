865e4b2db39d261775311d421012d2ef9546e941
shockleyje <shockleyje@g.cofc.edu>
2016-03-04 20:27:37 -0500

    bug fixes

--- OS5/hw5.c
+++ OS5/hw5.c
@@ -54,53 +54,67 @@ int main( int argc, char** argv ) {
       time in seconds = 0.0761
 
   */
+
+    //  1. Create the following variables:
    // - pointer to an array of pthread structures
   pthread_t *threads;
-  int i,        //    - counting variables for loops
-  threadid = 0, //    - thread id
-  exit_status,  //    - exit status of joined thread
-  numthreads,   //    - number of threads to run
+
+  int i,            //    - counting variables for loops
+  thread_id = 0,   //    - thread id
+  exit_status,    //    - exit status of joined thread
+  num_threads,   //    - number of threads to run
   err;          //    - error code for exit
   double time_elapsed;
 
   struct rlimit max_threads;
   struct timespec start, end;
 
-  //check for too little args
+  pthread_mutex_t count_mutex;
+
+  //check for too few args
   if (argc < 3)
   {
     exit(1);
   }
 
-  //grab parameters from command line args
-  numthreads = atoi(argv[1]);
+  //  2. Get number of threads input by user from argv[1]
+  num_threads = atoi(argv[1]);
+  //  3. Get number of iterations input by user from argv[2]
   numits = atoi(argv[2]);
 
+  //  4. Get the maximum number of threads the OS can create (hint: getrlimit function)
   getrlimit(RLIMIT_NPROC, &max_threads);
 
-  if (numthreads > max_threads.rlim_cur)
+  if (num_threads > max_threads.rlim_cur)
   {
     //too many threads
     exit(1);
   }
 
   //allocate space for threads
-  threads = (pthread_t*) malloc(sizeof(pthread_t) * numthreads);
+  //  6. Allocate an array of pthread structures using number of threads input by user (see step 2)
+  threads = (pthread_t*) malloc(sizeof(pthread_t) * num_threads);
 
   //start the clock
+  //  7. Get start time of simulation and store in time struct
   clock_gettime(CLOCK_REALTIME, &start);
 
   //create the threads
-  for (i = 0; i < numthreads; i++)
+  //  8. Use a loop to create a pthread for each index position in pthread array (created in step 5), if an error occurs during thread creation - exit simulation program immediately (hint: pthread_create function)
+  for (i = 0; i < num_threads; i++)
   {
-    if ((err = pthread_create(&(threads[i]), NULL, &th_routine, (void*) threadid++)) != 0)
+    pthread_mutex_lock()
+    if ((err = pthread_create(&(threads[i]), NULL, &th_routine, (void*) thread_id++)) != 0)
     {
       //thread killed it
       exit(err);
     }
+
   }
+
   //join the threads
-  for (i = 0; i < numthreads; i++)
+  //  9. Use a loop to join each pthread in created in the pthread array, if an error occurs during thread join operation - exit simulation program immediately (hint: pthread_join function)
+  for (i = 0; i < num_threads; i++)
   {
     if ((err = pthread_join(threads[i], NULL)) != 0)
     {
@@ -109,12 +123,26 @@ int main( int argc, char** argv ) {
     }
   }
 
-  //stop the clock
+  //  10. Get stop time of simulation and store in time struct
   clock_gettime(CLOCK_REALTIME, &end);
-  //determine elapsed time
   time_elapsed = mydifftime(&start, &end);
 
-  printf("mc value of pi: %.6f\nvalue of count: %.0f\ntime in seconds: %.4f\n\n", (4 * gcount/(numits * numthreads)), gcount, time_elapsed / 1000000000);
+  //print data
+  printf("monte carlo value of PI: %.6f\n
+          value of count: %.0f\n
+          time in seconds: %.4f\n\n", 
+          (4 * gcount/(numits * num_threads)), gcount, time_elapsed / 1000000000);
+
+  //  12. Free memory allovalue of countcated for pthread array
+  free(threads);
+
+  //    13. Call pthread terminate function (hint: pthread_exit function) 
+  for (i = 0; i < num_threads; i++)
+  {
+    pthread_join(threads[i], NULL)
+  }
+
+  //  14. exit simulation program
   return 0;
 
 } // end main function
