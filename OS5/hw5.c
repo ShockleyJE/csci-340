7dff5568c28d8b5fa2a767e1f32f99d062ebb406
shockleyje <shockleyje@g.cofc.edu>
2016-03-04 22:21:22 -0500

    turning in

--- OS5/hw5.c
+++ OS5/hw5.c
@@ -9,6 +9,8 @@
 double gcount;    // global counter -- a double to handle large sums (billions+)
 long numits;      // global variable for number of iterations (see step 3 below)
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 int main( int argc, char** argv ) {
 
   gcount = 0;
@@ -63,84 +65,68 @@ int main( int argc, char** argv ) {
   thread_id = 0,   //    - thread id
   exit_status,    //    - exit status of joined thread
   num_threads,   //    - number of threads to run
-  err;          //    - error code for exit
+  error;          //    - error code for exit
   double time_elapsed;
 
   struct rlimit max_threads;
   struct timespec start, end;
 
-  pthread_mutex_t count_mutex;
-
-  //check for too few args
-  if (argc < 3)
-  {
-    exit(1);
-  }
-
   //  2. Get number of threads input by user from argv[1]
   num_threads = atoi(argv[1]);
+
   //  3. Get number of iterations input by user from argv[2]
   numits = atoi(argv[2]);
 
   //  4. Get the maximum number of threads the OS can create (hint: getrlimit function)
   getrlimit(RLIMIT_NPROC, &max_threads);
 
+  //  5. If are number of threads > maximum OS threads then goto 14, else goto step 6
   if (num_threads > max_threads.rlim_cur)
   {
-    //too many threads
     exit(1);
   }
 
-  //allocate space for threads
   //  6. Allocate an array of pthread structures using number of threads input by user (see step 2)
   threads = (pthread_t*) malloc(sizeof(pthread_t) * num_threads);
 
-  //start the clock
   //  7. Get start time of simulation and store in time struct
   clock_gettime(CLOCK_REALTIME, &start);
 
-  //create the threads
-  //  8. Use a loop to create a pthread for each index position in pthread array (created in step 5), if an error occurs during thread creation - exit simulation program immediately (hint: pthread_create function)
+  //  8. Use a loop to create a pthread for each index position in pthread array (created in step 5)
   for (i = 0; i < num_threads; i++)
   {
-    pthread_mutex_lock()
-    if ((err = pthread_create(&(threads[i]), NULL, &th_routine, (void*) thread_id++)) != 0)
+
+    if ((error = pthread_create(&(threads[i]), NULL, (void*) th_routine, (void*) thread_id++)) != 0)
     {
-      //thread killed it
-      exit(err);
+      //if an error occurs during thread creation - exit simulation program immediately
+      exit(error);
     }
 
   }
 
-  //join the threads
-  //  9. Use a loop to join each pthread in created in the pthread array, if an error occurs during thread join operation - exit simulation program immediately (hint: pthread_join function)
+  //  9. Use a loop to join each pthread in created in the pthread array
   for (i = 0; i < num_threads; i++)
   {
-    if ((err = pthread_join(threads[i], NULL)) != 0)
+    if ((error = pthread_join(threads[i], NULL)) != 0)
     {
-      //thread join killed it
-      exit(err);
+      //if an error occurs during thread join operation - exit simulation program immediately
+      exit(error);
     }
   }
 
-  //  10. Get stop time of simulation and store in time struct
+  //  10. Get stop time of simulation
   clock_gettime(CLOCK_REALTIME, &end);
+  //  store in time struct
   time_elapsed = mydifftime(&start, &end);
 
   //print data
-  printf("monte carlo value of PI: %.6f\n
-          value of count: %.0f\n
-          time in seconds: %.4f\n\n", 
-          (4 * gcount/(numits * num_threads)), gcount, time_elapsed / 1000000000);
+  printf("monte carlo value of PI: %.6f\nvalue of count: %.0f\ntime in seconds: %.4f\n\n", (4 * gcount/(numits * num_threads)), gcount, time_elapsed / 1000000000);
 
   //  12. Free memory allovalue of countcated for pthread array
   free(threads);
 
-  //    13. Call pthread terminate function (hint: pthread_exit function) 
-  for (i = 0; i < num_threads; i++)
-  {
-    pthread_join(threads[i], NULL)
-  }
+  //  13. Call pthread terminate function (hint: pthread_exit function) 
+  //  NOTE: is it considered best practice to terminate a thread from main?
 
   //  14. exit simulation program
   return 0;
